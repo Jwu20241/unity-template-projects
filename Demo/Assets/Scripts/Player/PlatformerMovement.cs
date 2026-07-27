2adfdae30dd21e0f7bc0f24dae1e7eac294d2433
cbailey <cbd45068@gmail.com>
2026-07-27 10:07:35 -0700

    Finished platforming comments

--- Demo/Assets/Scripts/Player/PlatformerMovement.cs
+++ Demo/Assets/Scripts/Player/PlatformerMovement.cs
@@ -13,6 +13,7 @@ using UnityEngine;
 public class PlatformerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
+    private bool _grounded;
 
     public float speed;
     public float jumpHeight;
@@ -21,27 +22,49 @@ public class PlatformerMovement : MonoBehaviour
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
+    // Mod
     void Update()
     {
         _rb2d.velocity = HorizontalSpeed();
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("jump");
+        if(Input.GetKeyDown(KeyCode.Space) && _grounded) {
             _rb2d.velocity = Jump();
         }
     }
 
+    // Calculates the horizontal speed for bidirectional movement
+    // Works with arrow keys and AD based on the 'speed' variable
     Vector2 HorizontalSpeed()
     {
         return new Vector2(Input.GetAxis("Horizontal") * speed, _rb2d.velocity.y);
     }
 
+    // Calculates the jump velocity based on the 'jumpHeight' variable
     Vector2 Jump()
     {
-        return Vector2.up * jumpHeight;
+        return new Vector2(_rb2d.velocity.x, jumpHeight);
+    }
+
+    // Detects when the player collides with a floor
+    // Modifies the grounded variable to prevent multiple jumps
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            _grounded = true;
+        }
+    }
+
+    // Detects when the player exits the floor
+    // Modifies the grounded variable to prevent multiple jumps
+    void OnCollisionExit2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            _grounded = false;
+        }
     }
 }
